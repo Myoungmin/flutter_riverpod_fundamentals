7617baa35a2dc51dc86c4349e1cfe8455b73ae23
AzureAD\고명민 <miung222@gmail.com>
2024-08-04 22:30:21 +0900

    AutoDispose Modifier 실습
    위젯에서 사용하지 않을 때 캐싱되어 있던 데이터가 삭제되어야 하는 프로바이더를 생성할 때 autoDispose 키워드를 사용한다.

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod_fundamentals/layout/default_layout.dart';
+import 'package:flutter_riverpod_fundamentals/screen/auto_dispose_modifier_screen.dart';
 import 'package:flutter_riverpod_fundamentals/screen/family_modifier_screen.dart';
 import 'package:flutter_riverpod_fundamentals/screen/future_provider_screen.dart';
 import 'package:flutter_riverpod_fundamentals/screen/notifier_provider_screen.dart';
@@ -55,6 +56,15 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: const Text('FamilyModifierScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                    builder: (_) => const AutoDisposeModifierScreen()),
+              );
+            },
+            child: const Text('AutoDisposeModifierScreen'),
           ),
         ],
       ),
