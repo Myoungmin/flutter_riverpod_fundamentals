813e371f77379cddb450a510cd68971d8b58b284
AzureAD\고명민 <miung222@gmail.com>
2024-08-04 13:24:58 +0900

    기본적인 페이지 구성

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod_fundamentals/layout/default_layout.dart';
+import 'package:flutter_riverpod_fundamentals/screen/state_provider_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      title: 'HomeScreen',
+      body: ListView(
+        children: [
+          ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                      builder: (_) => const StateProviderScreen()),
+                );
+              },
+              child: const Text('StateProviderScreen')),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screen/state_provider_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod_fundamentals/layout/default_layout.dart';
+
+class StateProviderScreen extends StatelessWidget {
+  const StateProviderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const DefaultLayout(
+      title: 'StateProviderScreen',
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
