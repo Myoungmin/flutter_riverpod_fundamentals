dde8db2b994cae0dd83fce3cd89dc992275323c4
AzureAD\고명민 <miung222@gmail.com>
2024-08-05 19:54:44 +0900

    Provider 안에서 여러 개의 Provider를 watch하는 방법
    여러 개의 프로바이더들을 그대로 새로운 프로바이더 안에 집어넣고 이 안에서 로직을 작성

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter_riverpod_fundamentals/screen/family_modifier_screen.dart
 import 'package:flutter_riverpod_fundamentals/screen/future_provider_screen.dart';
 import 'package:flutter_riverpod_fundamentals/screen/listen_provider_screen.dart';
 import 'package:flutter_riverpod_fundamentals/screen/notifier_provider_screen.dart';
+import 'package:flutter_riverpod_fundamentals/screen/provider_screen.dart';
 import 'package:flutter_riverpod_fundamentals/screen/select_provider_screen.dart';
 import 'package:flutter_riverpod_fundamentals/screen/state_provider_screen.dart';
 import 'package:flutter_riverpod_fundamentals/screen/stream_provider_screen.dart';
@@ -83,6 +84,14 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: const Text('SelectProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (_) => const ProviderScreen()),
+              );
+            },
+            child: const Text('ProviderScreen'),
           ),
         ],
       ),
