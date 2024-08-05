374455e204f115ec957254c2f111b7830ec37f86
AzureAD\고명민 <miung222@gmail.com>
2024-08-05 20:25:40 +0900

    observer안에 등록하면 하위에 있는 모든 프로바이더들의 동작을 기록할 수 있다.
    didUpdateProvider
    didAddProvider
    didDisposeProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod_fundamentals/riverpod/provider_observer.dart';
 import 'package:flutter_riverpod_fundamentals/screen/home_screen.dart';
 
 void main() {
-  runApp(const ProviderScope(
-    child: MaterialApp(
+  runApp(ProviderScope(
+    observers: [
+      Logger(),
+    ],
+    child: const MaterialApp(
       home: HomeScreen(),
     ),
   ));
